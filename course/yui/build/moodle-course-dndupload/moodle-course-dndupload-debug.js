f6be530fc8de332c64fbf49f9a5c6e95d0b64218
Andrew Nicols <andrew@nicols.co.uk>
2013-07-03 18:18:52 +0100

    more dnd

--- course/yui/build/moodle-course-dndupload/moodle-course-dndupload-debug.js
+++ course/yui/build/moodle-course-dndupload/moodle-course-dndupload-debug.js
@@ -31,12 +31,15 @@ var SELECTORS = {
     section_mod: 'ul.section',
     sections_mods: 'li.section.main ul.section',
     section_types: 'li.section, li.main',
-    preview_element: '.dndupload-preview'
+    preview_element: '.dndupload-preview',
+    dnduploader: '.dndupload-loaded'
 },
     CSS = {
+    dnduploader: 'dndupload-loaded',
     preview_hide: 'dndupload-hidden',
     preview_over: 'dndupload-over'
 },
+    LOGNAME = 'moodle-course-dndupload',
     DNDUPLOAD;
 
 DNDUPLOAD = function() {
@@ -46,24 +49,27 @@ DNDUPLOAD = function() {
 
 Y.extend(DNDUPLOAD, Y.Base, {
     uploadqueue: [],
-
-    // TODO fix
-
-    showstatus: false,
-    previews_established: false,
-
     currentsection: null,
-
     entercount: 0,
 
+    // TODO fix
+    previews_established: false,
+
     initializer: function() {
+        if (Y.one(Y.config.doc.body).hasClass(SELECTORS.dnduploader)) {
+            Y.log('dndupload already loaded. Exiting', 'warn', LOGNAME);
+            return;
+        }
+
         // Check whether this browser supports drag-and-drop upload.
-        if (!this.browser_supported()) {
+        if (!Y.Moodle.course.dnduploadloader.browser_supported()) {
+            Y.log('Browser not supported. Exiting', 'warn', LOGNAME);
             return;
         }
 
         // Nothing to upload to - exit early.
         if (!Y.one(SELECTORS.sections)) {
+            Y.log('Not sections to upload to. Exiting', 'warn', LOGNAME);
             return;
         }
 
@@ -74,25 +80,16 @@ Y.extend(DNDUPLOAD, Y.Base, {
         Y.delegate('drop',      this.drop,      Y.config.doc, SELECTORS.sections, this);
 
         // Add the status message.
-        if (this.showstatus) {
+        if (this.get('showstatus')) {
             this.add_status_div();
         }
+
+        // Add the dnduploader class to the body to prevent it from being loaded again.
+        Y.one(Y.config.doc.body).addClass(CSS.dnduploader);
     },
 
-    /**
-     * Check whether the browser has the required functionality
-     *
-     * @method browser_supported
-     * @return {Boolean} Whether the user's browser supports drag-and-drop uploading of files.
-     */
-    browser_supported: function() {
-        if (typeof FileReader === 'undefined') {
-            return false;
-        }
-        if (typeof FormData === 'undefined') {
-            return false;
-        }
-        return true;
+    add_status_div: function() {
+        Y.log(this.get('statusmessage'));
     },
 
     add_preview_to_section: function(section) {
@@ -101,7 +98,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
         }
 
         if (typeof this.previewnode === "undefined") {
-            Y.log("Creating new preview node");
+            Y.log("Creating new preview node", 'info', LOGNAME);
             this.previewnode = Y.Node.create(
                 '<li class="dndupload-preview dndupload-hidden">' +
                     '<div class="mod-indent">' +
@@ -417,7 +414,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
         var type = this.check_drag(e),
             section = this.get_section(e.currentTarget);
 
-        Y.log("In the drop");
+        Y.log("In the drop", 'info', LOGNAME);
         this.hide_preview_element();
 
         if (!type) {
@@ -443,7 +440,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
                 if (index === 'length') {
                     continue;
                 }
-                Y.log("Checking index " + index, 'info', 'moodle-course-dndupload');
+                Y.log("Checking index " + index, 'info', LOGNAME);
                 this.handle_file(files[index], section);
             }
         } else {
@@ -468,7 +465,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
     handle_item: function(type, contents, section) {
         if (type.get('handlers').length === 0) {
             // Nothing to handle this - should not have got here
-            Y.log("Item with no handlers was passed", "error", "moodle-course-dndupload");
+            Y.log("Item with no handlers was passed", "error", LOGNAME);
             return;
         }
 
@@ -837,7 +834,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
         resel.one('.dndupload-progress-outer').remove();
 
         if (responseobject.content) {
-            Y.log("Creating a new label", "info", "moodle-course-dndupload");
+            Y.log("Creating a new label", "info", LOGNAME);
             var activityinstance = Y.Node.create('<div class="activityinstance" ></div>'),
                 wrapper = Y.Node.create('<div />'),
                 content = Y.Node.create(responseobject.content);
@@ -1063,10 +1060,7 @@ Y.extend(DNDUPLOAD, Y.Base, {
         if (!modsel) {
             // Create the above 'ul' if it doesn't exist.
             // TODO Rewrite - yuck!!!
-            modsel = Y.Node.create(
-                    '<ul class="section img-text">' +
-                    '</ul>'
-                );
+            modsel = Y.Node.create( '<ul class="section img-text"/>');
             var contentel = section.get('children').pop();
             var brel = contentel.get('children').pop();
             contentel.insertBefore(modsel, brel);
@@ -1100,6 +1094,15 @@ Y.extend(DNDUPLOAD, Y.Base, {
         showstatus: {
             value: 1
         },
+        statusmessage: {
+            value: {
+                identifier: 'dndstatus',
+                component: 'moodle'
+            },
+            getter: function(val) {
+                return M.util.get_string(val.identifier, val.component);
+            }
+        },
         url: {
             value: M.cfg.wwwroot + '/course/dndupload.php'
         }
@@ -1111,9 +1114,9 @@ Y.Moodle.course.dndupload.init = function(config) {
 };
 
 
-
 }, '@VERSION@', {
     "requires": [
+        "moodle-course-dndupload-loader",
         "node",
         "event",
         "json",
---
+++ course/yui/build/moodle-course-dnduploadloader/moodle-course-dnduploadloader.js
@@ -0,0 +1,63 @@
+YUI.add('moodle-course-dnduploadloader', function (Y, NAME) {
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adds the ability to perform drag-and-drop upload of files into a course.
+ *
+ * @module moodle-course-dndupload
+ * @package    core
+ * @subpackage course
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+Y.namespace('Moodle.course.dnduploadloader');
+
+var LOGNAME = 'moodle-course-dnduploadloader';
+
+Y.Moodle.course.dnduploadloader = {
+    init: function(config) {
+
+        if (!this.browser_supported()) {
+            return false;
+        }
+
+        Y.use('moodle-course-dndupload', function() {
+            Y.Moodle.course.dndupload.init(config);
+        });
+    },
+
+    /**
+     * Check whether the browser has the required functionality.
+     * This is also checked in the dndupload-loader before-hand but we
+     * check again in case this module was loaded separately.
+     *
+     * @method browser_supported
+     * @return {Boolean} Whether the user's browser supports drag-and-drop uploading of files.
+     */
+    browser_supported: function() {
+        if (typeof FileReader === 'undefined') {
+            return false;
+        }
+        if (typeof FormData === 'undefined') {
+            return false;
+        }
+        return true;
+    }
+};
+
+
+}, '@VERSION@');
