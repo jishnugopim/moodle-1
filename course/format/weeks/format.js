b0d463596c2d8d5766e6c92d008e5a0d0ee68302
Andrew Nicols <andrew@nicols.co.uk>
2013-12-23 14:26:11 +0800

    merge

--- course/format/weeks/format.js
+++ course/format/weeks/format.js
@@ -5,27 +5,6 @@ M.course = M.course || {};
 M.course.format = M.course.format || {};
 
 /**
- * Get sections config for this format
- *
- * The section structure is:
- * <ul class="weeks">
- *  <li class="section">...</li>
- *  <li class="section">...</li>
- *   ...
- * </ul>
- *
- * @return {object} section list configuration
- */
-M.course.format.get_config = function() {
-    return {
-        container_node : 'ul',
-        container_class : 'weeks',
-        section_node : 'li',
-        section_class : 'section'
-    };
-};
-
-/**
  * Swap section
  *
  * @param {YUI} Y YUI3 instance
