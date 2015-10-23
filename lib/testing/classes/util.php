352e60fd7073aba61fc242aa81bfbbb7c667d62e
Andrew Nicols <andrew@nicols.co.uk>
2015-10-23 11:58:31 +0800

    before

--- lib/testing/classes/util.php
+++ lib/testing/classes/util.php
@@ -685,7 +685,7 @@ abstract class testing_util {
                 if (isset($empties[$table])) {
                     // table was not modified and is empty
                 } else {
-                    $DB->delete_records($table, null);
+                    $DB->delete_records($table, array());
                 }
                 $summation['delete'] += (float) microtime_diff($changetimestart, microtime());
                 continue;
@@ -725,7 +725,7 @@ abstract class testing_util {
             $summation['delete2'] += (float) $timetaken;
 
             $changetimestart = microtime();
-            $DB->delete_records($table, null);
+            $DB->delete_records($table, array());
             foreach ($data[$table] as $record) {
                 $DB->import_record($table, $record, false, true);
             }
