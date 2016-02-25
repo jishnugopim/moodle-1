03bb8f3d41d546a4fc0e9c56791e9be5c196a2eb
Andrew Nicols <andrew@nicols.co.uk>
2016-02-25 12:22:52 +0800

    MDL-53172 javascript: Stop requiring body manipulation on load

--- lib/external/externallib.php
+++ lib/external/externallib.php
@@ -406,6 +406,7 @@ class core_external extends external_api {
                 'editlabel' => new external_value(PARAM_NOTAGS, 'label for editing element', VALUE_OPTIONAL),
                 'type' => new external_value(PARAM_ALPHA, 'type of the element (text, toggle, select)', VALUE_OPTIONAL),
                 'options' => new external_value(PARAM_RAW, 'options of the element, format depends on type', VALUE_OPTIONAL),
+                'displayinlink' => new external_value(PARAM_BOOL, 'Whether to display in the link', VALUE_OPTIONAL),
             )
         );
     }
