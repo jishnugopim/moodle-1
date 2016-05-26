64a2f49ba039b977285e1126fb3c83684931eca9
Andrew Nicols <andrew@nicols.co.uk>
2016-05-26 13:57:31 +0800

    MDL-54734 user: Add tests to demonstrate multi-user issues

--- lib/tests/user_test.php
+++ lib/tests/user_test.php
@@ -404,4 +404,26 @@ class core_user_testcase extends advanced_testcase {
         $this->setExpectedException('coding_exception', 'Invalid property requested, or the property does not has a default value.');
         core_user::get_property_default('firstname');
     }
+
+    /**
+     * Ensure that the noreply user is not cached.
+     */
+    public function test_get_noreply_user() {
+        global $CFG;
+
+        // Create a new fake language 'xx' with the 'noreplyname'.
+        $langconfig = "<?php\n\$string['noreplyname'] = 'XXX';";
+        $langfolder = $CFG->dataroot . '/lang/xx';
+        check_dir_exists($langfolder);
+        file_put_contents($langfolder . '/moodle.php', $langconfig);
+
+        $CFG->lang='en';
+        $enuser = \core_user::get_noreply_user();
+
+        $CFG->lang='xx';
+        $xxuser = \core_user::get_noreply_user();
+
+        $this->assertNotEquals($enuser, $xxuser);
+    }
+
 }
